3bb8506b399c8109bc196d001b83a0560140c0f0
naty meitav <naty.meitav@Gmail.com>
2020-08-28 13:09:52 +0300

    Add Constructor to Intake.

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -1,4 +1,15 @@
 package frc.robot.subsystems.Intake;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.Solenoid;
+import frc.robot.Constants;
 public class Intake {
+    private TalonSRX talonSRX;
+    private Solenoid piston;
+
+    public Intake(){
+        talonSRX = new TalonSRX(Constants.Intake.talonSRX);
+        talonSRX.setInverted(Constants.Intake.isInverted);
+        Solenoid piston = new Solenoid(Constants.Intake.piston);
+    }
 }
