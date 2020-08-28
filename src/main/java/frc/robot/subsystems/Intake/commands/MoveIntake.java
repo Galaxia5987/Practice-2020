b775b49b219b21dfb54f983fbd1d859639700eee
naty meitav <naty.meitav@Gmail.com>
2020-08-28 13:46:37 +0300

    Add command methods to MoveIntake.

--- src/main/java/frc/robot/subsystems/Intake/commands/MoveIntake.java
+++ src/main/java/frc/robot/subsystems/Intake/commands/MoveIntake.java
@@ -1,4 +1,31 @@
 package frc.robot.subsystems.Intake.commands;
 
-public class MoveIntake {
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake.Intake;
+
+public class MoveIntake extends CommandBase{
+    private Intake intake;
+    public MoveIntake(Intake intake) {
+        intake = intake;
+    }
+
+    @Override
+    public void initialize() {
+        intake.togglePiston();
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
 }
