188368f17eb9c001759f9cdf398eceaa9047306d
naty meitav <naty.meitav@Gmail.com>
2020-08-28 13:19:56 +0300

    Add State enum to Intake.

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -7,15 +7,21 @@ import frc.robot.Constants;
 public class Intake {
     private TalonSRX talonSRX;
     private Solenoid piston;
+    private State state;
 
     public Intake(){
         talonSRX = new TalonSRX(Constants.Intake.talonSRX);
         talonSRX.setInverted(Constants.Intake.isInverted);
-        Solenoid piston = new Solenoid(Constants.Intake.piston);
+        piston = new Solenoid(Constants.Intake.piston);
+        state = State.CLOSED;
     }
 
     public void setPower(double power){
         talonSRX.set(ControlMode.PercentOutput,power);
     }
-    
+
+    public enum State{
+        OPENED,
+        CLOSED;
+    }
 }
