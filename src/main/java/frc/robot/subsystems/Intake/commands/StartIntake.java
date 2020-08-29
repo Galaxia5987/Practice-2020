640c934a15109954ef0f49e56f0ba646f74f7b46
naty <naty.meitav@Gmail.com>
2020-08-29 14:21:37 +0300

    Add constructor to StartIntake.

--- src/main/java/frc/robot/subsystems/Intake/commands/StartIntake.java
+++ src/main/java/frc/robot/subsystems/Intake/commands/StartIntake.java
@@ -1,4 +1,31 @@
 package frc.robot.subsystems.Intake.commands;
 
-public class StartIntake {
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake.Intake;
+
+public class StartIntake extends CommandBase{
+    private Intake intake;
+    
+    public StartIntake(Intake i) {
+        intake = i;
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
