b054819a7ba760f28e2dec4d7f14c911e6ea0df2
naty <naty.meitav@Gmail.com>
2020-09-11 12:19:27 +0300

    Add constructor to Shoot Command.

--- src/main/java/frc/robot/subsystems/Shooter/commands/Shoot.java
+++ src/main/java/frc/robot/subsystems/Shooter/commands/Shoot.java
@@ -1,4 +1,13 @@
 package frc.robot.subsystems.Shooter.commands;
 
-public class Shoot {
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Shooter.Shooter;
+
+public class Shoot extends CommandBase {
+    private Shooter shooter;
+    private int power;
+    public Shoot(Shooter shooter,int power) {
+        this.shooter = shooter;
+        this.power = power;
+    }
 }
