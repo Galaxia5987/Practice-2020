d3e5527cadd0e746ff5508d9119f211860ca8ca9
naty <naty.meitav@Gmail.com>
2020-09-11 12:30:26 +0300

    Assign button A on the controller to Shoot command.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,8 +10,10 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.ExampleSubsystem.ExampleSubsystem;
-import frc.robot.subsystems.ExampleSubsystem.commands.ExampleCommand;
+import frc.robot.subsystems.Shooter.Shooter;
+import frc.robot.subsystems.Shooter.commands.Shoot;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -20,10 +22,9 @@ import frc.robot.subsystems.ExampleSubsystem.commands.ExampleCommand;
  * (including subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  public Intake intake = new Intake();
+  public Shooter shooter = new Shooter();
   public XboxController Xbox = new XboxController(1);
   public JoystickButton a = new JoystickButton(Xbox, XboxController.Button.kA.value);
-  public JoystickButton b = new JoystickButton(Xbox, XboxController.Button.kB.value);
   // The robot's subsystems and commands are defined here...
 
 
@@ -42,8 +43,7 @@ public class RobotContainer {
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    a.whileHeld(new EatCommand(intake, 0.5));
-    b.whenPressed(new Chomp(Intake, Intake.state.OPEN))
+    a.whileHeld(new Shoot(shooter, 0.5));
   }
 
 
