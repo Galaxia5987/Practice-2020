45ffe66aaa72a96b9a8965001e3f715cf3503529
naty <naty.meitav@Gmail.com>
2020-09-11 11:48:48 +0300

    Add SetPower method to Shooter SubSystem.

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj.Victor;
@@ -10,6 +11,7 @@ public class Shooter extends SubsystemBase {
     private TalonFX main;
     private VictorSPX side1;
     private VictorSPX side2;
+
     public Shooter(){
         main = new TalonFX(Constants.Shooter.Talon);
         main.setInverted(Constants.Shooter.isInverted);
@@ -19,5 +21,9 @@ public class Shooter extends SubsystemBase {
 
         side2 = new VictorSPX(Constants.Shooter.victor2);
         side2.follow(main);
+    }
+
+    public void setPower (double power){
+        main.set(ControlMode.PercentOutput,power);
     }
 }
