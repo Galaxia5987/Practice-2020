8387d97f347097be2dcf7a04cff9bb11c27b8856
naty <naty.meitav@Gmail.com>
2020-09-11 11:39:11 +0300

    Add Constructor to Shooter subSystem.

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,6 +1,23 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Shooter extends SubsystemBase {
+    private TalonFX main;
+    private VictorSPX side1;
+    private VictorSPX side2;
+    public Shooter(){
+        main = new TalonFX(Constants.Shooter.Talon);
+        main.setInverted(Constants.Shooter.isInverted);
+
+        side1 = new VictorSPX(Constants.Shooter.victor1);
+        side1.follow(main);
+
+        side2 = new VictorSPX(Constants.Shooter.victor2);
+        side2.follow(main);
+    }
 }
