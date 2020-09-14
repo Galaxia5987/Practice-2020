10171d167072772604fd61fc98d05ebb9224431a
naty <naty.meitav@Gmail.com>
2020-09-14 19:04:58 +0300

    Document Intake Subsystem.

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -7,31 +7,47 @@ import frc.robot.Constants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase{
-    private TalonSRX talonSRX;
-    private Solenoid piston;
-    private State state;
+    private TalonSRX talonSRX; // the Intake's motor
+    private Solenoid piston; // the Intake's pistons
+    private State state; // the pistons's state
 
+    /**
+     * Constructor
+     */
     public Intake(){
         talonSRX = new TalonSRX(Constants.Intake.talonSRX);
-        talonSRX.setInverted(Constants.Intake.isInverted);
+        talonSRX.setInverted(Constants.Intake.isInverted);// set talonSRX inverted (by constant)
         piston = new Solenoid(Constants.Intake.piston);
         state = State.CLOSED;
     }
 
+    /**
+     * @param power - power output (%)
+     * this function sets motor's power
+     */
     public void setPower(double power){
         talonSRX.set(ControlMode.PercentOutput,power);
     }
 
+    /**
+     * this function activates the piston
+     */
     public void openPiston(){
         piston.set(true);
         state = State.OPENED;
     }
 
+    /**
+     * this function deactivates the piston
+     */
     public void closePiston(){
         piston.set(false);
         state = State.CLOSED;
     }
 
+    /**
+     * this function toggles the piston's state (opened --> closed || closed --> opened)
+     */
     public void togglePiston(){
         if(state == State.OPENED)
             closePiston();
@@ -44,7 +60,8 @@ public class Intake extends SubsystemBase{
 
     }
 
-    public enum State{
+    
+    public enum State{//the piston optional states
         OPENED,
         CLOSED;
     }
