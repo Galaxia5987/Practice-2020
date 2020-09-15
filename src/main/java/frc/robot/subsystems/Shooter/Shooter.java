824107a5d8950673e87c7e81939d149829bcf827
naty <naty.meitav@Gmail.com>
2020-09-15 18:02:40 +0300

    Add UnitModel to Shooter Subsystem.

--- src/main/java/frc/robot/subsystems/Shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter/Shooter.java
@@ -5,16 +5,15 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Ports;
+import frc.robot.subsystems.UnitModel;
 
 public class Shooter extends SubsystemBase {
-    private TalonFX talonFX = new TalonFX(Constants.Shooter.TALON);// connect talonFX to it's motor (by constant)
-    private VictorSPX victorSPX1 = new VictorSPX(Constants.Shooter.VICTOR1);// connect VictorSPX1 to it's motor (by constant);
-    private VictorSPX getVictorSPX2 = new VictorSPX(Constants.Shooter.VICTOR2);// connect VictorSPX2 to it's motor (by constant);
+    private TalonFX talonFX = new TalonFX(Ports.Shooter.MASTER);// connect talonFX to it's motor (by constant)
+    private VictorSPX victorSPX1 = new VictorSPX(Ports.Shooter.SLAVE1);// connect VictorSPX1 to it's motor (by constant);
+    private VictorSPX getVictorSPX2 = new VictorSPX(Ports.Shooter.SLAVE2);// connect VictorSPX2 to it's motor (by constant);
     private final UnitModel rpsUnitModel = new UnitModel(Constants.Shooter.TICKS_PER_ROTATION);
 
-    /**
-     * Contractor
-     */
     public Shooter(){
         talonFX.setInverted(Constants.Shooter.IS_INVERTED);// set motor inverted (by constant)
 
