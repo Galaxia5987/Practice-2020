be5f374ae51cf077f87d7725cc33ce20f77a37db
naty <naty.meitav@Gmail.com>
2020-09-18 10:36:26 +0300

    Remove unnecessary documentation.

--- src/main/java/frc/robot/subsystems/Shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter/Shooter.java
@@ -9,18 +9,21 @@ import frc.robot.Ports;
 import frc.robot.subsystems.UnitModel;
 
 public class Shooter extends SubsystemBase {
-    private TalonFX talonFX = new TalonFX(Ports.Shooter.MASTER);// connect talonFX to it's motor (by constant)
-    private VictorSPX victorSPX1 = new VictorSPX(Ports.Shooter.SLAVE1);// connect VictorSPX1 to it's motor (by constant);
-    private VictorSPX getVictorSPX2 = new VictorSPX(Ports.Shooter.SLAVE2);// connect VictorSPX2 to it's motor (by constant);
-    private final UnitModel rpsUnitModel = new UnitModel(Constants.Shooter.TICKS_PER_ROTATION);
+    private TalonFX talonFX = new TalonFX(Ports.Shooter.MASTER);
+    private VictorSPX victorSPX1 = new VictorSPX(Ports.Shooter.SLAVE1);
+    private VictorSPX getVictorSPX2 = new VictorSPX(Ports.Shooter.SLAVE2);
+    private final UnitModel UnitModel = new UnitModel(Constants.Shooter.TICKS_PER_ROTATION);
 
     public Shooter(){
+        /**
+         * Class constructor
+         */
         talonFX.setInverted(Constants.Shooter.IS_INVERTED);// set motor inverted (by constant)
 
-        victorSPX1.follow(talonFX);// make side1 follow main motor
+        victorSPX1.follow(talonFX);
         victorSPX1.setInverted(Constants.Shooter.IS_INVERTED);// set motor inverted (by constant)
 
-        getVictorSPX2.follow(talonFX);// make side2 follow main motor
+        getVictorSPX2.follow(talonFX);
         getVictorSPX2.setInverted(Constants.Shooter.IS_INVERTED);// set motor inverted (by constant)
 
         talonFX.config_kP(Constants.Shooter.PID_SLOT,Constants.Shooter.KP);
@@ -29,11 +32,12 @@ public class Shooter extends SubsystemBase {
         talonFX.config_kF(Constants.Shooter.PID_SLOT,Constants.Shooter.KF);
     }
 
-    /**
-     * @param power: the power output (%)
-     * this function activates Shooter's motors
-     */
+
     public void setPower (double power){
+        /**
+         * @param power: the power output (%)
+         * this function activates Shooter's motors
+         */
         talonFX.set(ControlMode.PercentOutput,power);
     }
 }
