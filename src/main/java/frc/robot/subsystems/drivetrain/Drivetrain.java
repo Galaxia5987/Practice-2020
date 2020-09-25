7b147326ebefe3ad0179b9fb6ea91799fdfe0aec
Saar <saarzi2004@gmail.com>
2020-09-25 10:22:02 +0300

    Create drivetrain subsystem.

---
+++ src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems.drivetrain;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+public class Drivetrain {
+    private final TalonFX drivetrainMaster1=new TalonFX(1);
+    private final TalonFX drivetrainMaster2=new TalonFX(2);
+    private final TalonFX drivetrainMaster3=new TalonFX(3);
+    private final TalonFX drivetrainMaster4=new TalonFX(4);
+}
