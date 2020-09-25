421d9e1461a0d2cd2ec85be37908fb66ccb0cdea
naty <naty.meitav@Gmail.com>
2020-09-25 10:57:55 +0300

    Rename Motors to master & slaves.

--- src/main/java/frc/robot/subsystems/Shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter/Shooter.java
@@ -9,31 +9,27 @@ import frc.robot.Ports;
 import frc.robot.subsystems.UnitModel;
 
 public class Shooter extends SubsystemBase {
-    private TalonFX talonFX = new TalonFX(Ports.Shooter.MASTER);
-    private VictorSPX victorSPX1 = new VictorSPX(Ports.Shooter.SLAVE1);
-    private VictorSPX getVictorSPX2 = new VictorSPX(Ports.Shooter.SLAVE2);
+    private TalonFX master = new TalonFX(Ports.Shooter.MASTER);
+    private VictorSPX slave1 = new VictorSPX(Ports.Shooter.SLAVE1);
+    private VictorSPX slave2 = new VictorSPX(Ports.Shooter.SLAVE2);
     private final UnitModel UnitModel = new UnitModel(Constants.Shooter.TICKS_PER_ROTATION);
 
     public Shooter(){
         /**
          * Class constructor
          */
-        talonFX.setInverted(Constants.Shooter.IS_INVERTED);
+        master.setInverted(Constants.Shooter.IS_INVERTED);
 
-        victorSPX1.follow(talonFX);
-        victorSPX1.setInverted(Constants.Shooter.IS_INVERTED);
+        slave1.follow(master);
+        slave1.setInverted(Constants.Shooter.IS_INVERTED);
 
-        getVictorSPX2.follow(talonFX);
-        getVictorSPX2.setInverted(Constants.Shooter.IS_INVERTED);
+        slave2.follow(master);
+        slave2.setInverted(Constants.Shooter.IS_INVERTED);
 
-        talonFX.config_kP(Constants.Shooter.PID_SLOT,Constants.Shooter.KP);
-        talonFX.config_kI(Constants.Shooter.PID_SLOT,Constants.Shooter.KI);
-        talonFX.config_kD(Constants.Shooter.PID_SLOT,Constants.Shooter.KD);
-        talonFX.config_kF(Constants.Shooter.PID_SLOT,Constants.Shooter.KF);
-    }
-
-    public int getVelocity(){
-        return talonFX.getSelectedSensorVelocity();
+        master.config_kP(Constants.Shooter.PID_SLOT,Constants.Shooter.KP);
+        master.config_kI(Constants.Shooter.PID_SLOT,Constants.Shooter.KI);
+        master.config_kD(Constants.Shooter.PID_SLOT,Constants.Shooter.KD);
+        master.config_kF(Constants.Shooter.PID_SLOT,Constants.Shooter.KF);
     }
 
     public void setPower (double power){
@@ -41,6 +37,6 @@ public class Shooter extends SubsystemBase {
          * this function activates Shooter's motors
          * @param power: the power output (%)
          */
-        talonFX.set(ControlMode.PercentOutput,power);
+        master.set(ControlMode.PercentOutput,power);
     }
 }
