f48387561660e79b73a22659c852c358026287fc
Saar <saarzi2004@gmail.com>
2020-09-25 11:38:32 +0300

    Create drivetrain subsystem.
    Getters and Setters for speed and get for acceleration.

--- src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -1,10 +1,50 @@
 package frc.robot.subsystems.drivetrain;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Robot;
 
-public class Drivetrain {
-    private final TalonFX drivetrainMaster1=new TalonFX(1);
-    private final TalonFX drivetrainMaster2=new TalonFX(2);
-    private final TalonFX drivetrainMaster3=new TalonFX(3);
-    private final TalonFX drivetrainMaster4=new TalonFX(4);
+public class Drivetrain extends SubsystemBase {
+    private final TalonFX drivetrainMasterRight1 = new TalonFX(1);
+    private final TalonFX drivetrainMasterRight2 = new TalonFX(2);
+    private final TalonFX drivetrainMasterLeft1 = new TalonFX(3);
+    private final TalonFX drivetrainMasterLeft2 = new TalonFX(4);
+
+    public Drivetrain() {
+        drivetrainMasterRight1.setInverted(true);
+        drivetrainMasterRight2.follow(drivetrainMasterRight1);
+        drivetrainMasterLeft1.setInverted(false);
+        drivetrainMasterLeft2.follow(drivetrainMasterLeft1);
+    }
+
+    public double getSpeedRight() {
+        return drivetrainMasterRight1.getSelectedSensorVelocity();
+    }
+
+    public double getSpeedLeft() {
+        return drivetrainMasterLeft1.getSelectedSensorVelocity();
+    }
+
+    public void setPower(double powerRight, double powerLeft) {
+        drivetrainMasterRight1.set(ControlMode.PercentOutput,powerRight);
+        drivetrainMasterLeft1.set(ControlMode.PercentOutput,powerLeft);
+    }
+
+    public void setVelocity(double velocityRight, double velocityLeft) {
+        drivetrainMasterRight1.set(ControlMode.Velocity,velocityRight);
+        drivetrainMasterLeft1.set(ControlMode.Velocity,velocityLeft);
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+
+    public double getAcceleration() {
+        return Robot.navx.getWorldLinearAccelY()* Constants.g;
+    }
+
+
 }
