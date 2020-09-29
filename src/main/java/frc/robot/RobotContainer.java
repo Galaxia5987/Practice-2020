78afcd106c616ef6f59c64aabde284777f2d9226
Saar <saarzi2004@gmail.com>
2020-09-29 21:35:27 +0300

    Create turret subsystem.
    TalonSRX motor. setAngle function. getSpeed, getPosition.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ExampleSubsystem.ExampleSubsystem;
-import frc.robot.subsystems.ExampleSubsystem.commands.ExampleCommand;
+//import frc.robot.subsystems.ExampleSubsystem.commands.ExampleCommand;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.drivetrain.commands.JoystickDrive;
 
--- src/main/java/frc/robot/subsystems/turret/Turret.java
+++ src/main/java/frc/robot/subsystems/turret/Turret.java
@@ -1,7 +1,58 @@
 package frc.robot.subsystems.turret;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.UnitModel;
 
-public class Turret {
-    private final TalonSRX turretMaster=new TalonSRX(1);
+public class Turret extends SubsystemBase {
+    private final TalonSRX turretMaster = new TalonSRX(1);
+    private UnitModel unitModel;
+
+    public Turret() {
+        turretMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, Constants.Turret.TALON_TIMEOUT);
+        unitModel = new UnitModel(Constants.Turret.ticksPerDegree);
+    }
+
+    public void setAngle(double angle) {
+        double addAngle = 0;
+        double myAngle = getPosition();
+        double option1 = angle - myAngle;
+        double option2 = -myAngle - (360 - angle);
+        double option3 = myAngle - angle;
+        double option4 = 360 - myAngle;
+        if (angle > myAngle) {
+            if (Math.abs(option1) < Math.abs(option2))
+                addAngle = option1;
+            else
+                addAngle = option2;
+        }
+        else {
+            if (Math.abs(option3) < Math.abs(option4))
+                addAngle = option3;
+            else
+                addAngle = option4;
+        }
+
+        turretMaster.set(ControlMode.Position, unitModel.toUnits(addAngle));
+    }
+
+    public double getSpeed() {
+        return unitModel.toVelocity(turretMaster.getSelectedSensorVelocity());
+    }
+
+    public double getPosition() {
+        return unitModel.toUnits(turretMaster.getSelectedSensorPosition());
+    }
+
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
 }
+
+
+
+
