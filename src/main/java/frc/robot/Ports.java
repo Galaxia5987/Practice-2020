9326bbb44ebe293da82a405747229cca8c99a6dd
Saar <saarzi2004@gmail.com>
2020-10-02 11:12:47 +0300

    Create Ports class.
    Create Drivetrain class in Ports. Define Ports for 4 drivetrain motors.

---
+++ src/main/java/frc/robot/Ports.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+public class Ports {
+    public static class Drivetrain {
+        public static final int MASTER_RIGHT = 1;
+        public static final int SLAVE_RIGHT = 2;
+        public static final int MASTER_LEFT = 3;
+        public static final int SLAVE_LEFT = 4;
+
+    }
+
+
+}
