72aa19cbcec2a3418be58be79747e6a72ad23357
Saar <saarzi2004@gmail.com>
2020-10-02 11:29:01 +0300

    Create turret subsystem.
    TalonSRX motor. setAngle function. getSpeed, getPosition.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,9 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ExampleSubsystem.ExampleSubsystem;
-import frc.robot.subsystems.ExampleSubsystem.commands.ExampleCommand;
+//import frc.robot.subsystems.ExampleSubsystem.commands.ExampleCommand;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.subsystems.drivetrain.commands.JoystickDrive;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
