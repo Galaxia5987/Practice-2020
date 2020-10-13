924cb68d12c81be254c3efa2770dad4c17616411
naty <naty.meitav@Gmail.com>
2020-10-13 16:35:55 +0300

    Move ports to Ports class.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,8 +17,5 @@ package frc.robot;
  */
 public final class Constants {
     public class Intake{
-        public static final int talonSRX = 0;
-        public static final int piston = 1;
-        public static final boolean isInverted = false;
     }
 }
