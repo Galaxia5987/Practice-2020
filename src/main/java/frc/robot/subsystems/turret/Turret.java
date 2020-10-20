ae562a862dd7f6b2250ff9ea9b2e1c80defa8ace
Segol <saarzi2004@gmail.com>
2020-10-20 16:10:55 +0300

    Document.

--- src/main/java/frc/robot/subsystems/turret/Turret.java
+++ src/main/java/frc/robot/subsystems/turret/Turret.java
@@ -12,6 +12,9 @@ public class Turret extends SubsystemBase {
     private final TalonSRX master = new TalonSRX(Ports.Turret.MASTER);
     private UnitModel unitModel;
 
+    /**
+     * Turret Constructor: Config feedback sensor, create new unit model, and config kP,kI and kD.
+     */
     public Turret() {
         master.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, Constants.TALON_TIMEOUT);
         unitModel = new UnitModel(Constants.Turret.TICKS_PER_DEGREE);
