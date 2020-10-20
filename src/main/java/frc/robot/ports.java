bf48bbdd82b0a53fbca6fdd4e3ff787faf14be35
itamar <itamar030305@gmail.com>
2020-10-20 17:40:20 +0300

    Organizing ports

--- src/main/java/frc/robot/ports.java
+++ src/main/java/frc/robot/ports.java
@@ -2,10 +2,11 @@ package frc.robot;
 
 public class ports {
     public static class Drivetrain{
-        public static final int RIGHT_SLAVE = 0;
-        public static final int RIGHT_MASTER = 1;
-        public static final int LEFT_SLAVE = 2;
-        public static final int LEFT_MASTER = 3;
+        public static final int RIGHT_MASTER = 0;
+        public static final int RIGHT_SLAVE = 1;
+        public static final int LEFT_MASTER = 2;
+        public static final int LEFT_SLAVE = 3;
+
 
         public static final boolean left_inverted = false;
         public static final boolean right_inverted = true;
