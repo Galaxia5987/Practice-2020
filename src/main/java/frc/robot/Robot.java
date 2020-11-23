bff23636b96edd880d0564dcb45ec74955e48644
adam <adam.vichik@gmail.com>
2020-11-23 20:20:15 +0200

    Delete line.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -67,7 +67,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
