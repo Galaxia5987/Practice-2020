a690e93da27d020ad56698625c130701d0a84332
adam <adam.vichik@gmail.com>
2020-11-23 20:20:56 +0200

    Change to minus.

--- src/main/java/frc/robot/subsystems/drivetrain/commands/JoystickDrive.java
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/JoystickDrive.java
@@ -33,8 +33,8 @@ public class JoystickDrive extends CommandBase {
      */
     @Override
     public void execute() {
-        rightPower = RobotContainer.r.getY();
-        leftPower = RobotContainer.l.getY();
+        rightPower = -RobotContainer.r.getY();
+        leftPower = -RobotContainer.l.getY();
         drivetrain.setPower(rightPower, leftPower);
     }
 
