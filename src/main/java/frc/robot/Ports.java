42ac163e94b37598aa491e3a78d4d6912df93543
adam <saarzi2004@gmail.com>
2020-11-27 12:57:23 +0200

    Fix shooter's ports & invert slaves & sensor_phase.

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -3,12 +3,12 @@ package frc.robot;
 public class Ports {
     public static final class Shooter {
         public static final boolean IS_INVERTED_MASTER = false;
-        public static final boolean IS_INVERTED_SLAVE1 = false;
-        public static final boolean IS_INVERTED_SLAVE2 = false;
-        public static final boolean SENSOR_PHASE = false;
+        public static final boolean IS_INVERTED_SLAVE1 = true;
+        public static final boolean IS_INVERTED_SLAVE2 = true;
+        public static final boolean SENSOR_PHASE = true;
 
-        public static final int MASTER = 0;
-        public static final int SLAVE1 = 1;
-        public static final int SLAVE2 = 2;
+        public static final int MASTER = 23;
+        public static final int SLAVE1 = 24;
+        public static final int SLAVE2 = 25;
     }
 }
